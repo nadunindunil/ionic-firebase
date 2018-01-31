911ca0dbefedb6c097f01c20e3262b715e7fc388
Nadun Indunil <nadun1indunil@gmail.com>
2018-01-31 22:54:50 +0530

    initial commit

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyB00oRHeGOS_AsR4eTwrFkKlxDbUGe8d_0",
+    authDomain: "gym-app-c404a.firebaseapp.com",
+    databaseURL: "https://gym-app-c404a.firebaseio.com",
+    projectId: "gym-app-c404a",
+    storageBucket: "gym-app-c404a.appspot.com",
+    messagingSenderId: "313887947258"
+};
