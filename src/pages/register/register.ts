cd8ba6e8bbcc3f1cb528d4d4af5235f4e6ce129c
Nadun Indunil <nadun1indunil@gmail.com>
2018-02-03 23:39:22 +0530

    build: now new users can add in to users table from authentication

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from "../../models/user";
 import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase } from 'angularfire2/database-deprecated';
 
 @IonicPage()
 @Component({
@@ -12,14 +13,15 @@ export class RegisterPage {
 
   user = {} as User;
 
-  constructor(private afAuth: AngularFireAuth,
+  constructor(private afAuth: AngularFireAuth, private adDB: AngularFireDatabase,
     public navCtrl: NavController, public navParams: NavParams) {
   }
 
   async register(user: User) {
     try {
       const result = await this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
-      console.log(result);
+      const fResult = await this.adDB.database.ref('/users').child(result.uid).set({ email: user.email });    
+      console.log(fResult);
     }
     catch (e) {
       console.error(e);
